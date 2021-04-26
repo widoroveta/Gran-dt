072c10bd41c1e0a025f9516277258b068a3b42f4
guido <widoroveta@gmail.com>
2021-04-26 07:33:58 -0300

    inicial version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,11 @@
 package com.company;
 
+import com.company.Class.Country;
 import com.company.Request.AllSportsApi;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -17,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         String appKey = "4f613aeef397a3ac7ed09e01692012de508c4f7f66277ac5a0eb798113e7081e";
         URL url = null;
-        List<String> stringList=new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
         HttpURLConnection con = null;
         String jsonInputString = "{\"country_key\": \"country_name\"}";
         try {
@@ -33,19 +38,40 @@ public class Main {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }try(BufferedReader br = new BufferedReader(
+            }
+            try (BufferedReader br = new BufferedReader(
                     new InputStreamReader(con.getInputStream(), "utf-8"))) {
                 StringBuilder response = new StringBuilder();
                 String responseLine = null;
-              //  while ((responseLine = br.readLine()) != null) {
-                  responseLine= br.readLine();
-               // }
+                //  while ((responseLine = br.readLine()) != null) {
+                responseLine = br.readLine();
+                // }
 
-              String d=responseLine.replaceAll("country_key","countryKey");
-              String t=d.replaceAll("country_name","countryName");
-              d=t.replaceAll("country_iso","countryIso");
-              t=d.replaceAll("country_logo","countryLogo");
-              System.out.println(t);
+                String d = responseLine.replaceAll("country_key", "countryKey");
+                String t = d.replaceAll("country_name", "countryName");
+                d = t.replaceAll("country_iso", "countryIso");
+                t = d.replaceAll("country_logo", "countryLogo");
+                d = t.substring(t.indexOf("["));
+                System.out.println(d);
+
+                ObjectMapper mapper = new ObjectMapper();
+
+                mapper.writerWithDefaultPrettyPrinter().writeValue(new File("cacho.json"), d.trim());
+               mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+                //mapper.writeValue(new File("messi.json"),new Country( 1, "car;ps","ads","sad"));
+                List<Country> countries = new ArrayList<>();
+countries.add(new Country(1,"sdadsa","21","sad"));
+countries.add(new Country(1,"sdadsa","21","sad"));
+countries.add(new Country(1,"sdadsa","21","sad"));
+countries.add(new Country(1,"sdadsa","21","sad"));
+countries.add(new Country(1,"sdadsa","21","sad"));
+countries.add(new Country(1,"sdadsa","21","sad"));
+mapper.writerWithDefaultPrettyPrinter().writeValue(new File("messi.json"),countries);
+        /*       countries = mapper.readValue(new File("cacho.json"), new TypeReference<List<Country>>() {
+                });-**/
+
+                System.out.println(countries.toString());
+
             }
 
         } catch (MalformedURLException e) {
